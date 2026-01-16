12f789596b8e4cdbd0972d6484eedbb2b6a7bc0d
김지호 <rlawl3383@gmail.com>
2026-01-16 10:42:56 +0900

    스토리북 설정 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,24 @@
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
   ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "..\\public"
-  ]
+  framework: "@storybook/nextjs-vite",
+  staticDirs: ["..\\public"],
+  async viteFinal(config) {
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+      tsconfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    );
+    return config;
+  },
 };
 export default config;
