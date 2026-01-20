4a596f5c420b3b0eb2441ab4c813bfcb1deb58be
김지호 <rlawl3383@gmail.com>
2026-01-20 17:31:31 +0900

    404 페이지 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,15 +2,18 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import styles from "../asset/404.module.css";
-
+import CommonButton from "@/atoms/CommonButton/CommonButton";
 const ErrorPage = () => {
   const router = useRouter();
 
   return (
     <div className={styles.notfoundRoot}>
       <div className={styles.notfoundWrapper}>
+        <div className={styles.notfoundBg}></div>
         <h1 className={styles.title}>페이지를 찾을 수 없습니다.</h1>
-        <button onClick={() => router.push("/auth")}>돌아가기</button>
+        <CommonButton theme="primary" onClick={() => router.back()}>
+          돌아가기
+        </CommonButton>
       </div>
     </div>
   );
